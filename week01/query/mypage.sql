3987ba493f9c92828bfe7376f4683581285304b0
hyeonkangkimm <kkhk75@gmail.com>
2025-09-28 15:02:04 +0900

    Apply .gitignore rules

--- week01/query/mypage.sql
+++
@@ -1,3 +0,0 @@
-SELECT  users.name , users.email , users.phone_num, users.point
-FROM users
-WHERE user_id = :userId;
--- week01/query/review.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO review(review_id, store_id, user_id, star, content, create_at, change_at)
-VALUES (:reviewId, :storeId, :userId, :star, :content, NOW(6), NULL);
